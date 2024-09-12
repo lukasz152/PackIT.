1075ab6915625091135b5eee55d848b1b3fc5694
Łukasz Zalewski <Lukasz.Zalewski@rossmann.com.pl>
2024-09-12 13:09:31 +0200

    Infrastructure layer

---
+++ src/PackIT.Infrastructure/EF/Models/LocalizationReadModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PackIT.Infrastructure.EF.Models;
+
+internal class LocalizationReadModel
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+    public static LocalizationReadModel Create(string value)
+    {
+        var splitLocalitzation = value.Split(',');
+        return new LocalizationReadModel
+        {
+            City = splitLocalitzation.First(),
+            Country = splitLocalitzation.Last()
+        };
+    }
+    public override string ToString()
+        => $"{City},{Country}";
+}
---
+++ src/PackIT.Infrastructure/EF/Models/PackingListReadModel.cs
@@ -0,0 +1,14 @@
+﻿using PackIT.Domain.ValueObjects;
+using PackIT.Infrastructure.EF.Models;
+
+namespace PackIT.Application.Dto
+{
+    internal class PackingListReadModel
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+        public string Name {  get; set; }
+        public LocalizationReadModel Localization { get; set; }
+        public ICollection<PackingItemReadModel> Items { get; set; }
+    }
+}
